eb7e28ad65e3055e9c8632b48730376f3490f7a1
Nacon <lager_91@hotmail.com>
2017-11-11 00:25:22 +0100

    get pulled son

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(720, 480), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
     
     while (window.isOpen())
     {
