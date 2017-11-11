bbd32dc89d4f23638e5a58063c17b4ea567ea399
kevso311 <kevso311@student.liu.se>
2017-11-11 12:36:36 +0100

    Adding character skeleton
    Check above, it's not that hard is it?

--- Character.h
+++ Character.h
@@ -1,3 +1,5 @@
+#include <string>
+#include <SFML/Graphics.hpp>
 #pragma once
 #ifndef CHARACTER_H
 #define CHARACHER_H
@@ -5,12 +7,15 @@
 class Character {
 
 public:
-
-
+	Character(std::string conf);
+	~Character() = default;
 
 private:
+	sf::Texture sheet;
 
-
+	int x{ 0 }, y{ 0 };
+	int startX{ 0 }, startY{ 0 };
+	int goX{ 0 }, goY{ 0 };
 
 };
 
